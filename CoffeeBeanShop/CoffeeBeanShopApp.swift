09c1a5e1b5502eea72c6bbdaa0937b0974fa01d4
Danil <karatelanalov@yandex.ru>
2025-07-04 21:45:24 +0300

    init

---
+++ CoffeeBeanShop/CoffeeBeanShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBeanShopApp.swift
+//  CoffeeBeanShop
+//
+//  Created by Данил Дьяченко on 04.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBeanShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeBeanShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeBeanShop
+//
+//  Created by Данил Дьяченко on 04.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
